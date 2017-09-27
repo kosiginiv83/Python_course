b6574a544b5cbc4e927c3f0769b259b21a7b2a6e
kosiginiv83 <kosiginiv83@ADMIN-LAPTOP>
2017-09-27 19:17:22 +0800

    Замечания по dz_2-3.py исправлены.

--- PY1_Lesson_2.3/dz_2-3.py
+++ PY1_Lesson_2.3/dz_2-3.py
@@ -1,71 +1,53 @@
 import json
 import chardet
-# from pprint import pprint
 
 
-def cp_identify(file):
+def file_load(file):
 	with open(file, 'rb') as f:
 		data = f.read()
 		result = chardet.detect(data)
-		cp = result['encoding']
-		return cp
+		s = data.decode(result['encoding'])
+		j = json.loads(s)
+		return j
 	
 
 def form_data_structure(files_list, chars_more):
 	data_list = list()
 	
 	for file in files_list:
-		cp = cp_identify(file)
-		with open(file, encoding=cp) as f:
-			words_dict = dict()
-			news_dscrptns = list()
-			
-			news_dict = json.load(f)
-			news_desc_list = news_dict['rss']['channel']['items']
-			for i in range(len(news_desc_list)):
-				words_list = news_desc_list[i]['description'].split(' ')
-				words_list_condition = [words_list[x] for x in 
-						range(len(words_list)) if 
-						len(words_list[x]) > chars_more]
-				news_dscrptns += words_list_condition
-			words_dict['file'] = file
-			words_dict['news'] = news_dscrptns
-			data_list.append(words_dict)
-			
-	top_ten_words(data_list, chars_more)
-		
+		words_dict = dict()
+		news_dscrptns = list()
 
-def top_ten_words(data_list, chars_more):
+		news_dict = file_load(file)
+		news_desc_list = news_dict['rss']['channel']['items']
+		for i in range(len(news_desc_list)):
+			words_list = news_desc_list[i]['description'].split(' ')
+			words_list_condition = [word for word in words_list if 
+					len(word) > chars_more]
+			news_dscrptns += words_list_condition
+		words_dict['file'] = file
+		words_dict['news'] = news_dscrptns
+		data_list.append(words_dict)
+	
 	print("\nТоп 10 часто встречающихся слов, длиннее {} "
 				"символов:".format(chars_more))
-				
-	for data in data_list:
-		top_ten_list = list()
+	top_ten_words(data_list)
 		
-		for i in range(10):
-			words_count_list = list()
-			max_words_list = list()
-			
-			words_list = data['news']
-			for word in words_list:
-				word_count = words_list.count(word)
-				words_count_list.append(word_count)
-			max_count = max(words_count_list)
-			max_word_index = words_count_list.index(max_count)
-			max_words_indexes = list(filter(lambda x: 
-					words_count_list[x] == max_count, 
-					range(len(words_list))))
-			for index in max_words_indexes:
-				max_words_list.append(words_list[index])
-			words_set = set(max_words_list)
-			for word in words_set:
-				top_ten_list.append(word)
-				while word in words_list:
-					words_list.remove(word)
-	
+
+def top_ten_words(data_list):
+	for data in data_list:
 		print('\n\tFile: ', data['file'])
-		for num, value in enumerate(top_ten_list[:10]):
-			print(num + 1, '-', value)
+		words = data['news']
+		freq = {}
+		for word in words:
+			if word not in freq:
+				freq[word] = 1
+			else:
+				freq[word] += 1
+		sorted_count_pairs = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+		top10 = sorted_count_pairs[:10]
+		for word, freq in top10:
+			print("Слово '{}' встретилось {} раз".format(word, freq))
 	
 	
 # Второй аргумент - число символов в слове - в данном 
