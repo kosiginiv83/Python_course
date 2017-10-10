e67eedb05e801ea11b7b7a57708e04c50a57b183
kosiginiv83 <kosiginiv83@ADMIN-LAPTOP>
2017-10-10 22:45:37 +0800

    dz_3.2

--- homework/vk_1.py
+++ homework/vk_1.py
@@ -1,77 +1,50 @@
 import requests
-from pprint import pprint
-import itertools
 import functools
+
 
 VERSION = '5.68'
 
-my_params = {
-	'v': VERSION,
-	'user_id': 251522831
-}
 
-response = requests.get('https://api.vk.com/method/friends.get', my_params)
-my_friends = response.json()
-# pprint(my_friends)
+def get_friends(user_id):
+    my_params = {
+        'v': VERSION,
+        'user_id': user_id
+    }
+    response = requests.get('https://api.vk.com/method/friends.get', my_params)
+    return response.json()
+
+
+my_friends = get_friends(251522831)
 my_friends_id_list = my_friends['response']['items']
-print('\n\tmy_friends_id_list: ', my_friends_id_list, '\n')
+print('\n\tmy_friends_id_list: ', my_friends_id_list)
 
-for friend_id in my_friends['response']['items']:
-	params = {
-		'v': VERSION,
-		'user_id': friend_id
-	}
-	friend_info_raw = requests.get('https://api.vk.com/method/users.get', params)
-	friend_info = friend_info_raw.json()
-	# print(friend_info)
+
+def get_user(user_id):
+    params = {
+        'v': VERSION,
+        'user_id': user_id
+    }
+    response = requests.get('https://api.vk.com/method/users.get', params)
+    return response.json()
+
 
 friends_friends_dict = {}
-for id in my_friends_id_list:
-	friend_params = {
-		'v': VERSION,
-		'user_id': id
-	}
-	friend_info_raw = requests.get('https://api.vk.com/method/users.get', friend_params)
-	friend_info = friend_info_raw.json()
-	friend_firstname = friend_info['response'][0]['first_name']
-	friend_lastname = friend_info['response'][0]['last_name']
-	fio = f"{friend_lastname} {friend_firstname}"
-	# print('\n', fio)
-	
-	response = requests.get('https://api.vk.com/method/friends.get', friend_params)
-	my_friend_friends = response.json()
-	# print(my_friend_friends)
-	
-	friends_friends_dict[fio] = set(my_friend_friends['response']['items'])
-	
-print(friends_friends_dict)
+print("\n\tFriends names:")
+for friend_id in my_friends_id_list:
+    friend_info = get_user(friend_id)
+    friend_firstname = friend_info['response'][0]['first_name']
+    friend_lastname = friend_info['response'][0]['last_name']
+    fio = f"{friend_lastname} {friend_firstname}"
+    print(fio)
+    my_friend_friends = get_friends(friend_id)
+    friends_friends_dict[fio] = set(my_friend_friends['response']['items'])
 
 
-print('\n\tIntersections\n')
-# set.inersection(other, ...)
-# int_per_pol = (set(friends_friends_dict['Перебейносов Алексей']) & 
-		# set(friends_friends_dict['Пологрудов Алексей']))
-int_list = list(friends_friends_dict.values())
-inter = functools.reduce(int_list[0].intersection, int_list[1:])
-
-# inter = itertools.starmap(int_list[0].intersection, range(1, len(int_list) - 1))
-# inter = int_list[0].intersection(int_list[x] for x in range(1, len(int_list) - 1))
-# inter = int_list[0].intersection(itertools.islice(int_list, 1, len(int_list) - 1))
-
-print(dir(inter))
-print('\n\t', inter)
-print(type(inter))
-
-
-for id in inter:
-	friend_params = {
-		'v': VERSION,
-		'user_id': id
-	}
-	friend_info_raw = requests.get('https://api.vk.com/method/users.get', friend_params)
-	friend_info = friend_info_raw.json()
-	friend_firstname = friend_info['response'][0]['first_name']
-	friend_lastname = friend_info['response'][0]['last_name']
-	fio = f"{friend_lastname} {friend_firstname}"
-	print('\n', fio)
-	
+print('\n\tFriends intersection')
+# Ищется пересечение только среди друзей друзей, чтобы для проверки
+# правильности работы кода выводился как минимум я.
+ids_list = list(friends_friends_dict.values())
+inter = functools.reduce(ids_list[0].intersection, ids_list[1:])
+print('\tUsers id', inter)
+for user_id in inter:
+    print(get_user(user_id))
