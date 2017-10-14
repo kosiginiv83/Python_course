75abab4c87a800ae0630d2461ab52d6680e09f06
kosiginiv83 <kosiginiv83@ADMIN-LAPTOP>
2017-10-14 22:34:49 +0800

    no message

---
+++ homework/3.4-currencies/travel.py
@@ -0,0 +1,28 @@
+import osa
+
+
+def length_convert(mi):
+    client = osa.Client('http://www.webservicex.net/length.asmx?WSDL')
+    res = client.service.ChangeLengthUnit(LengthValue=mi,
+                                     fromLengthUnit='Miles',
+                                     toLengthUnit='Kilometers')
+    print('Total:', round(res, 2), 'km')
+
+
+def main(file):
+    length_list = []
+    with open(file) as f:
+        for line in f:
+            item = line.split()
+            length_list.append(item[1])
+    print(length_list)
+    length_list = [float(length_list[x].replace(',', '')) for x in
+                   range(len(length_list))]
+    print(length_list)
+    mi = sum(length_list)
+    print('Total:', mi, 'mi')
+    length_convert(mi)
+
+
+if __name__ == '__main__':
+    main('travel.txt')
