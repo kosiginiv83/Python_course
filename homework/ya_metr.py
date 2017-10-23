6a7da6046ca3385b48f885bdc421e5d0f8cd8543
kosiginiv83 <kosiginiv83@ADMIN-LAPTOP>
2017-10-23 16:41:12 +0800

    no message

---
+++ homework/ya_metr.py
@@ -0,0 +1,17 @@
+import requests
+from pprint import pprint
+
+TOKEN = 'AQAAAAAazgOnAASYKJ4J3j8TvUnVmJ-pENTHj6Y'
+
+params = {
+    'oauth_token': TOKEN
+}
+
+response = requests.get('https://api-metrika.yandex.ru/management/v1/counters',
+                        params)
+print(response.status_code)
+pprint(response.headers)
+pprint(response.json())
+
+class Get_metrs:
+    
