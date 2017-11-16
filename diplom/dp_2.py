9a0b6f4a64162c8297261dabdfc93136ed85b8e9
kosiginiv83 <kosiginiv83@ADMIN-LAPTOP>
2017-11-16 22:42:49 +0800

    no message

--- diplom/dp_2.py
+++ diplom/dp_2.py
@@ -7,7 +7,7 @@ VERSION = '5.68'
 
 with open('inf0.json') as f:
     data = json.load(f)
-    pprint(data)
+    #pprint(data)
     TOKEN = data[0]['token']
     USER_ID = data[0]['user_id']
 
@@ -40,21 +40,42 @@ def user_fio(friend_info):
     #print(friend_info)
 
 
-if __name__ == '__main__':
-    user_info = get_data(USER_ID, 'users')
-    print(user_info)
+def get_friends_groups():
+    friends_groups_list = []
+    # user_info = get_data(USER_ID, 'users')
+    # print(user_info)
     friends_info = get_data(USER_ID, 'friends')
-    #print(friends_ids)
-    groups_dict = {}
     friends_ids = friends_info['response']['items']
+    #print(friends_ids)
     for friend_id in friends_ids:
         friend_info = get_data(friend_id, 'users')
         user_fio(friend_info)
         friend_groups_raw = get_data(friend_id, 'groups')
         #print(friend_groups_raw)
-        friend_groups_ids = friend_groups_raw['response']['items']
-        for group_id in friend_groups_ids:
-            friend_group_info = get_group_info(group_id)
-            #pprint(friend_group_info)
-            #print(friend_group_info['response'][0]['name'])
+        
+        if 'response' in friend_groups_raw:
+            friend_groups_ids = friend_groups_raw['response']['items']
+            friends_groups_list += friend_groups_ids
+            """
+            Выводит название группы.
+            for group_id in friend_groups_ids:
+                friend_group_info = get_group_info(group_id)
+                #pprint(friend_group_info)
+                #print(friend_group_info['response'][0]['name'])
+            """
+        elif 'error' in friend_groups_raw:
+            print(friend_groups_raw['error']['error_msg'])
+        
         print('==================================')
+    friends_groups_set = set(friends_groups_list)
+    print(friends_groups_set)
+    
+
+if __name__ == '__main__':
+    try:
+        get_friends_groups()
+    except:
+        print("\tAn Error Occured")
+    #finally:
+    
+    
